e49551a0d0f01c1673ef28ef0adebbb4b451a647
Althaf Muhammed Umer <61247025+althafMuhammedUmer@users.noreply.github.com>
2026-04-15 16:07:13 +0400

    updated changes

--- pages/agenda/index.tsx
+++ pages/agenda/index.tsx
@@ -7,7 +7,7 @@ export default function index() {
   return (
     <div>
       <div className="black-replacer-nav"></div>
-      <HeaderBanner2026
+      {/* <HeaderBanner2026
         heading="Agenda"
         subheading="Explore our agenda for a comprehensive overview of conference sessions and key conversations. "
         className="col-lg-10"
@@ -18,6 +18,15 @@ export default function index() {
             <AgendaComponent eventId="2ff15d64-aa6c-4351-8b27-2828cd79ba4b" />
           </div>
         </div>
+      </div> */}
+
+      <div className="container py-5 text-center">
+        <h1 className="fw-bold text-dark mb-3">
+          Agenda <span className="text-primary">Coming Soon</span>
+        </h1>
+        <p className="text-muted fs-5">
+          Get ready for insightful talks, networking opportunities, and more.
+        </p>
       </div>
     </div>
   );
